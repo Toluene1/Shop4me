5cb947a30277d6c5161ce385f7734c20763b7791
Toluene1 <adaramoti111@gmail.com>
2022-12-16 01:06:03 +0100

    fixed cart icon positioning

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -25,12 +25,14 @@ const Navbar = () => {
             <div className=" col-sm-2 "><h2>Shop4me</h2></div>
 
             <div className="mx-2 col-sm-7 d-none d-sm-block text-center ">
-                <button className={`btn btn-light mx-3 p-0 text-white bg-${theme}`}><Link to={"/"} className='btn btn-link link-dark text-decoration-none text-warning'>Home</Link></button>
-                <button className={`btn btn-light mx-3 p-0 text-white bg-${theme}`}><Link to={"/login"} className='btn btn-link link-dark text-decoration-none text-warning'>Login</Link></button>
-                <button className={`btn btn-light mx-3 p-0 text-white bg-${theme}`}><Link to={"/products"} className='btn btn-link link-dark text-decoration-none text-warning'>Products</Link></button>
+                <button className={`btn btn-light mx-3 p-0 text-white bg-${theme}`}><Link to={"/"} className='btn btn-link link-dark text-decoration-none text-warning fw-bold'>Home</Link></button>
+                <button className={`btn btn-light mx-3 p-0 text-white bg-${theme}`}><Link to={"/login"} className='btn btn-link link-dark text-decoration-none text-warning fw-bold'>Login</Link></button>
+                <button className={`btn btn-light mx-3 p-0 text-white bg-${theme}`}><Link to={"/products"} className='btn btn-link link-dark text-decoration-none text-warning fw-bold'>Products</Link></button>
             </div>
             <div className="col-sm-1">
-                <Link to={"/cart"} className='btn btn-link link-dark text-decoration-none  text-dark'><span className="" style={{ fontSize: "30px" }}><ShoppingCartOutlined /></span></Link><span className="position-absolute top-4" style={{ fontSize: "15px" }}>{Cart.length}</span>
+                <Link to={"/cart"} className='btn btn-link link-dark text-decoration-none  text-dark'><button type="button" style={{ height: "40px" }} className="btn btn-warning position-relative">
+                    <span style={{ fontSize: "37px", position: "relative", top: "-25px" }}><ShoppingCartOutlined /></span> <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-black text-warning">{Cart.length}</span>
+                </button></Link>
             </div>
             <div className="col-sm-1">
                 <button className={`m-2 mx-3 px-2 btn btn-light text-warning bg-${theme}`} onClick={ChangeColor}>{
